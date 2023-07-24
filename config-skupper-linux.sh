50f1f161033aa2af9b38e02ad689e24bb1b3df13
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2023-07-24 12:24:24 -0300

    v2

--- config-skupper-linux.sh
+++ config-skupper-linux.sh
@@ -1,5 +1,5 @@
 skupper init --enable-console --enable-flow-collector --console-auth unsecured
-skupper gateway init --config simple_docker.yaml --type docker
+skupper gateway init --type docker
 skupper expose service database --address database --port 5432 --protocol tcp
 # N funciona no mac
 skupper gateway bind database localhost 5432
